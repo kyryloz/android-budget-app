bd9aa6ef4c50d37248e5a5101a8b5463a4c60a5b
zak <zak@swingpulse.com>
2016-05-07 16:08:36 +0300

    Add transaction

--- app/src/main/java/ua/com/zak/budgetswing/activity/AddTransactionActivity.java
+++ app/src/main/java/ua/com/zak/budgetswing/activity/AddTransactionActivity.java
@@ -2,7 +2,9 @@ package ua.com.zak.budgetswing.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import butterknife.Bind;
 import ua.com.zak.budgetswing.R;
@@ -18,8 +20,19 @@ public class AddTransactionActivity extends BaseActivity {
 
         setSupportActionBar(mToolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        ActionBar supportActionBar = getSupportActionBar();
+
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
--- core/src/main/java/ua/com/zak/budgetswing/core/mvp/presenter/TransactionsPresenter.java
+++ core/src/main/java/ua/com/zak/budgetswing/core/mvp/presenter/TransactionsPresenter.java
@@ -1,13 +1,19 @@
 package ua.com.zak.budgetswing.core.mvp.presenter;
 
+import javax.inject.Inject;
+
 import ua.com.zak.budgetswing.core.di.ApplicationComponent;
 import ua.com.zak.budgetswing.core.mvp.view.TransactionsView;
 import ua.com.zak.budgetswing.core.navigator.NavigationBundle;
+import ua.com.zak.budgetswing.core.navigator.Navigator;
 
 /**
  * @author zak <zak@swingpulse.com>
  */
 public class TransactionsPresenter extends Presenter<TransactionsView> {
+
+    @Inject
+    Navigator mNavigator;
 
     public TransactionsPresenter(TransactionsView view) {
         super(view);
@@ -24,6 +30,6 @@ public class TransactionsPresenter extends Presenter<TransactionsView> {
     }
 
     public void addNewTransaction(NavigationBundle navigationBundle) {
-
+        mNavigator.openAddTransactionScreen(navigationBundle);
     }
 }
