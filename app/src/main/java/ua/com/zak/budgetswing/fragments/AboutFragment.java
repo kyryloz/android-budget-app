edc16cdf50450141805a55f3634fdd0e3ced1c42
zak <zak@swingpulse.com>
2016-05-07 17:31:22 +0300

    Impl home and about

--- app/src/main/java/ua/com/zak/budgetswing/fragments/AboutFragment.java
+++ app/src/main/java/ua/com/zak/budgetswing/fragments/AboutFragment.java
@@ -1,7 +1,12 @@
 package ua.com.zak.budgetswing.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import butterknife.Bind;
 import ua.com.zak.budgetswing.R;
 import ua.com.zak.budgetswing.core.mvp.presenter.AboutPresenter;
 import ua.com.zak.budgetswing.core.mvp.view.AboutView;
@@ -10,6 +15,9 @@ import ua.com.zak.budgetswing.core.mvp.view.AboutView;
  * @author zak <zak@swingpulse.com>
  */
 public class AboutFragment extends BasePresenterFragment<AboutPresenter> implements AboutView {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
 
     public static Fragment newInstance() {
         return new AboutFragment();
@@ -23,5 +31,11 @@ public class AboutFragment extends BasePresenterFragment<AboutPresenter> impleme
     @Override
     protected AboutPresenter createPresenter() {
         return new AboutPresenter(this);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initToolbarToggle(mToolbar);
     }
 }
