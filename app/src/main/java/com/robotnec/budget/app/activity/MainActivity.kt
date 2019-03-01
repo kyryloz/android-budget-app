c388caa0cfb0c496a727227cc3ed33ef5413fa1c
Kyrylo Zapylaiev <zapylaev@gmail.com>
2019-03-01 14:23:21 +0200

    Add bottom menu

--- app/src/main/java/com/robotnec/budget/app/activity/MainActivity.kt
+++ app/src/main/java/com/robotnec/budget/app/activity/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.robotnec.budget.app.activity
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.robotnec.budget.R
 import com.robotnec.budget.core.mvp.presenter.MainPresenter
 import com.robotnec.budget.core.mvp.view.MainView
@@ -23,42 +20,8 @@ class MainActivity : BasePresenterActivity<MainPresenter>(), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setSupportActionBar(toolbar)
-
         val navController = navigationHostFragment.findNavController()
 
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.homeFragment,
-                R.id.categoriesFragment,
-                R.id.transactionsFragment,
-                R.id.aboutFragment,
-                R.id.accountsFragment
-        ), drawer)
-
-        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
-        NavigationUI.setupWithNavController(toolbar, navController, drawer)
-    }
-
-    override fun onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
